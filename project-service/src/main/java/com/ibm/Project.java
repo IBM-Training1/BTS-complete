ff349a894b321cfe7bc4f95edddc331dc29d53f9
unknown <ibmjavanh16@iiht.tech>
2021-03-30 15:56:14 +0530

    project class modified

--- project-service/src/main/java/com/ibm/Project.java
+++ project-service/src/main/java/com/ibm/Project.java
@@ -3,27 +3,33 @@ package com.ibm;
 import javax.validation.constraints.NotNull;
 
 public class Project {
+	@NotNull
 	private String id;
 	@NotNull
 	private String name;
 	@NotNull
 	private String type;
-	
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getType() {
 		return type;
 	}
+
 	public void setType(String type) {
 		this.type = type;
 	}
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
