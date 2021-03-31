41ed19b1cfebf67a04100945ac2609e6eec2c9e3
unknown <ibmjavanh02@iiht.tech>
2021-03-31 10:24:32 +0530

    Added transaction manager

--- project-service/src/main/java/com/ibm/ProjectService.java
+++ project-service/src/main/java/com/ibm/ProjectService.java
@@ -5,13 +5,16 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 @Service
 public class ProjectService {
 	@Autowired
 	ProjectRepository projectRepository;
-
+	@Transactional
 	public String createProject(Project project) {
 		Project savedProject = projectRepository.save(project);
+		if(project != null)
+			throw new RuntimeException();
 		return savedProject.getId();
 	}
 
