a106c899409528635c8fb427a91f0f178247d3ce
unknown <ibmjavanh16@iiht.tech>
2021-03-31 12:09:28 +0530

    BugController created

--- bug-service/src/main/java/com/ibm/BugController.java
+++ bug-service/src/main/java/com/ibm/BugController.java
@@ -1,5 +1,53 @@
 package com.ibm;
 
-public class BugController {
+import java.util.List;
+import java.util.Optional;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BugController {
+	@Autowired
+	BugService bugService;
+
+	private void validateModel(Errors bindingResult) {
+		if (bindingResult.hasErrors()) {
+			throw new IllegalArgumentException("something went wrong, please try again");
+		}
+	}
+
+	@PostMapping("/bug")
+	String createBug(@RequestBody @Valid Bug bug, BindingResult bindingResult) {
+		validateModel(bindingResult);
+		System.out.println(bug);
+		return bugService.createBug(bug);
+	}
+
+	@GetMapping("/bug/{id}")
+	Optional<Bug> getBug(@PathVariable("id") String bugId) {
+		return bugService.getBug(bugId);
+	}
+
+	@GetMapping("/bug")
+	List<Bug> getBugs() {
+		return bugService.getBugs();
+	}
+
+	@PutMapping("/bug/{id}")
+	void updateBug(@RequestBody @Valid Bug bug, @PathVariable("id") String bugId, BindingResult bindingResult) {
+		validateModel(bindingResult);
+		System.out.println(bugId);
+		bug.setId(bugId);
+		bugService.updateBug(bug);
+	}
 }
