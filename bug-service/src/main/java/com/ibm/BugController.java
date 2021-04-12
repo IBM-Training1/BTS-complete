89154a75a8af13e7beba977dab4ef0ed06dee6e3
unknown <ibmjavanh16@iiht.tech>
2021-04-12 13:53:10 +0530

    changes

--- bug-service/src/main/java/com/ibm/BugController.java
+++ bug-service/src/main/java/com/ibm/BugController.java
@@ -1,7 +1,7 @@
 package com.ibm;
 
 import java.util.List;
-import java.util.Optional;
+
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 public class BugController {
@@ -52,15 +54,19 @@ public class BugController {
 	 * @param bugId
 	 * @return zero or matchingId
 	 */
-	@GetMapping("/bug/{id}")
-	Optional<Bug> getBugById(@PathVariable("id") String bugId) {
-		return bugService.getBugById(bugId);
-	}
-
 	@GetMapping("/bug")
-	List<Bug> getBugs() {
-		return bugService.getBugs();
+	List<Bug> getAllBugs(){
+		return bugService.getAllBugs();
 	}
+//	@GetMapping("/bug/{id}")
+//	Optional<Bug> getBugById(@PathVariable("id") String bugId) {
+//		return bugService.getBugById(bugId);
+//	}
+//
+//	@GetMapping("/bug")
+//	List<Bug> getBugs() {
+//		return bugService.getBugs();
+//	}
 
 	/**
 	 * updates the changes in Bug
