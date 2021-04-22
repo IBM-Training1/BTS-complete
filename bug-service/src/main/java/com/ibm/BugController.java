8bcb9d2a28383ed0d8346449ef478ea0316327f5
unknown <ibmjavanh05@iiht.tech>
2021-04-22 14:02:32 +0530

    Added delete function

--- bug-service/src/main/java/com/ibm/BugController.java
+++ bug-service/src/main/java/com/ibm/BugController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -107,5 +108,11 @@ public class BugController {
 		bug.setId(bugId);
 		bugService.updateBugStatus(bug);
 
+	}
+	
+	@DeleteMapping("/bug/{id}")
+	void deleteOrder(@PathVariable("id") String bugId) {
+		System.out.println(bugId);
+		bugService.deleteBug(bugId);
 	}
 }
